1aff13531178dd7b4f239794b5065fd490942e12
mujtabax229 <mujtabax229@gmail.com>
2026-05-29 13:23:12 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bayt_shwayti_v1/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
